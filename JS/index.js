0a5b3a5153115548b48fe9fb99b393fe6243a386
Andejy <104570789+Andejy@users.noreply.github.com>
2022-10-13 18:43:10 -0400

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,47 +1,36 @@
 import data from "../data.json" assert { type: "json" };
 
+// En esta zona se llama el contenedor de las barras y se optienen los datos de el JSON
+// Se utilizan los datos de el JSON para crear las barras y se les da un tamaño y color y se crea un array al cual se le agrgan los datos de el JSON
+
 let charsecontainer = document.querySelector(".chart__bars-container");
-
-
-let values =[]
+let values = [];
 
 data.forEach((e) => {
-  values.push(e.amount)
+  values.push(e.amount);
   charsecontainer.innerHTML += `<div class="chart__bar">
   <div class="chart__bar--label">$${e.amount}</div>
   <div class="chart__bar--day">${e.day}</div>
 </div> `;
 });
 
+// Aqui se busca el valor maximos que nosa da el JSON y se le da un tamaño a las barras
 
-
-
-
-console.log(values)
-
-let maxValue = Math.max( ...values),
-maxHeight = 150;
-
-console.log(maxValue)
-
+let maxValue = Math.max(...values),
+  maxHeight = 150;
 
 let bars = document.querySelectorAll(`.chart__bar`);
 bars = [...bars];
 
-
-
+// En es forEach se itera por el arreglo creado a partir de todas la barras llamdas desde el doom y se le da un tamaño y color
 
 bars.forEach((bar) => {
+  let nuevoValor = parseFloat(bar.childNodes[1].innerText.slice(1));
+  let actualHeight = (nuevoValor * maxHeight) / maxValue;
 
-  let nuevoValor = parseFloat( bar.childNodes[1].innerText.slice(1));
-  let actualHeight = (nuevoValor * maxHeight) / maxValue
-
-  if (nuevoValor== maxValue) {
-
-  bar.style.backgroundColor = ` rgb(118, 181, 188)`
-
-};
-  
+  if (nuevoValor == maxValue) {
+    bar.style.backgroundColor = ` rgb(118, 181, 188)`;
+  }
 
   bar.style.height = `${actualHeight}px`;
 
